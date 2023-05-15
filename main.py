6465f4509844fd148f91f7668242f50eaf578cb1
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2023-05-14 18:18:51 -0600

    Mejora interfaz

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ root = tk.Tk()
 root.title("Mi compilador")
 root.geometry("1280x720")
 
+# Configuración del tema oscuro
+root.configure(bg="#1E1E1E")
+root.option_add('*foreground', 'white')
+root.option_add('*background', '#1E1E1E')
+root.option_add('*activeBackground', '#333333')
+root.option_add('*activeForeground', 'white')
+
 # Función para compilar el código
 def compilar():
     codigo = codigo_base.get("1.0", "end-1c") # Obtiene el código base
@@ -39,16 +46,31 @@ def compilar():
         resultado.insert("1.0", resultado_parser)
 
 # Configuración del área de código base
-codigo_base = tk.Text(root, height=20, width=80)
-codigo_base.pack(side=tk.LEFT)
+codigo_frame = tk.Frame(root, bg="#333333")
+codigo_label = tk.Label(codigo_frame, text="Código", bg="#333333", fg="white", font=("Arial", 12, "bold"))
+codigo_label.pack(side=tk.TOP, pady=10)
+codigo_base = tk.Text(codigo_frame, height=20, width=80, bg="#1E1E1E", fg="white", insertbackground="white", font=("Consolas", 12))
+codigo_base.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
 
 # Configuración del botón de compilación
-boton_compilar = tk.Button(root, text="Compilar", command=compilar)
-boton_compilar.pack()
+boton_compilar = tk.Button(root, text="Compilar", command=compilar, bg="#333333", fg="white", font=("Arial", 12, "bold"))
+boton_compilar.pack(pady=10)
 
 # Configuración del área de resultados
-resultado = tk.Text(root, height=20, width=80)
-resultado.pack(side=tk.RIGHT)
+resultado_frame = tk.Frame(root, bg="#333333")
+resultado_label = tk.Label(resultado_frame, text="Resultados", bg="#333333", fg="white", font=("Arial", 12, "bold"))
+resultado_label.pack(side=tk.TOP, pady=10)
+resultado = tk.Text(resultado_frame, height=20, width=80, bg="#1E1E1E", fg="white", insertbackground="white", font=("Consolas", 12))
+resultado.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
 
-# Inicia el bucle de eventos
+
+codigo_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+boton_compilar.pack(pady=10)
+resultado_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+codigo_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+boton_compilar.pack(pady=10)
+resultado_frame.pack(side=tk.LEFT, padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+
 root.mainloop()
